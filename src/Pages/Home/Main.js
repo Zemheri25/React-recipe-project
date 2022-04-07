6280a04eff609fb781529d8e26d59215c9fda2b0
Zemheri25 <omerzemheri14@gmail.com>
2022-04-08 00:57:33 +0300

    changed

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -1,43 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Details from "../../components/Details";
-import { useState } from "react";
 
 import "./Main.css";
 import Home from "../Home1/Home";
 import About from "../About/About";
 import NotFound from "../../components/NotFound";
-import { GiHamburgerMenu } from "react-icons/gi";
+
 
 function Main({ removeUser }) {
-  const [open, setOpen] = useState(true);
 
+  
   return (
     <Router>
       <nav className="navbar">
-        <Link to="/home" className="logbutton h1" onClick={() =>setOpen(false)}>
+        <Link to="/home" className="logbutton h1">
           Zemheri Recipe
         </Link>
-        {open && (
-          <div className="div2">
-            <Link to="/about" className="logbutton" onClick={() => setOpen(false)}>
-              About
-            </Link>
-            <a
-              href="https://github.com/Zemheri25 "
-              className="logbutton"
-              target={"_blank"}
-            >
-              GITHUB
-            </a>
+        <div className="div2">
+          <Link to="/about" className="logbutton">
+            About
+          </Link>
+          <a
+            href="https://github.com/Zemheri25 "
+            className="logbutton"
+            target={"_blank"}
+          >
+            GITHUB
+          </a>
 
-            <button className="logbutton button" onClick={() => removeUser()}>
-              Log Out
-            </button>
-          </div>
-        )}
-        <GiHamburgerMenu className="hamburger" onClick={() => setOpen(!open)} />
+          <button className="logbutton button" onClick={() => removeUser()}>
+            Log Out
+          </button>
+        </div>
+        
       </nav>
+
+      
 
       <Routes>
         <Route path="/home" element={<Home />} />
